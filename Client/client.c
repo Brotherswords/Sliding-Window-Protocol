b84474ec259f53ede795102bf1fc3cd7231505a8
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2022-10-28 20:56:12 -0600

    Sending information to server, just not in the right format nor order

--- Client/client.c
+++ Client/client.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include <unistd.h>
+#include <ctype.h>
 
+#define BUFFSIZE 30
 
-
-int getFileLength(FILE* fp);
+int sendData(int sd, struct sockaddr_in server_address, struct sockaddr_in from_address, socklen_t from_address_len);
 
 int main(int argc, char *argv[]){
     int sd;
     struct sockaddr_in server_address;
-    char filename[100];
+    struct sockaddr_in from_address;
+    socklen_t from_address_len = sizeof(from_address);
     int portNumber;
     char serverIP[29];
-    FILE *inputFile;
     int rc = 0;
 
     if (argc < 3) {
@@ -24,8 +26,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    sd = socket(AF_INET, SOCK_STREAM, 0);
-
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
     portNumber = strtol(argv[2], NULL,10);
     strcpy(serverIP, argv[1]);
 
@@ -39,102 +40,54 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-
-    while(1){
-        //Making sure that filename doesnt have extra characters
-        memset(filename,0,100);
-        printf("What is the name of the file you'd like to send?\n");
-        //Take user input on the file name and store it!
-        scanf("%s", filename);
-        //Create an input file string of only that length and copy it over to prevent extra characters from being sent over the network.
-        char inputFileName[strlen(filename)];
-        memcpy(inputFileName, filename, strlen(filename));
-        inputFileName[strlen(filename)] = '\0';
-        printf("filename to send it '%s'\n", inputFileName);
-        printf("filename size to send it '%lu'\n", strlen(inputFileName));
-        //Get and convert the size of the file to network order.
-        int sizeOfFileName = strlen(inputFileName);
-        int converted_sizeOfFileName = ntohs(sizeOfFileName);
-
-        //WRITE STATEMENT
-        //Getting ready to send the size of the file name in bytes
-        rc = write(sd, &converted_sizeOfFileName, sizeof(converted_sizeOfFileName));
-        printf("wrote %d bytes to send the filename size\n", rc);
-        if (rc < 0) {
-            perror("error writing");
-        }
-
-        //Check if DONE is entered, and if so write the corresponding file size over to make sure the 
-        //reads on the server size understand that this is not a file to be transferred, and then terminate
-        //the connection. 
-        int isDone = -1;
-        isDone = strcmp(inputFileName, "DONE");
-        if (isDone == 0) {
-            int bytesToWrite = 0;
-            int convertedBytesToWrite = htonl(bytesToWrite);
-            rc = write(sd, &convertedBytesToWrite, sizeof(convertedBytesToWrite));
-            rc = write(sd, &inputFileName, sizeof(inputFileName));
-            printf("Connection Closed :)");
-            return 0;
-        };    
-        //Getting ready to read from the file. 
-        if((inputFile = fopen(filename, "rb")) == NULL) {
-            printf("cannot open input file %s\n", filename);
-        }
-
-        //Get and convert the size of the file to network length
-        int bytesToWrite = getFileLength(inputFile);
-        int convertedBytesToWrite = htonl(bytesToWrite);
-        rc = write(sd, &convertedBytesToWrite, sizeof(convertedBytesToWrite));
-        printf("wrote %d bytes to send the file length\n", bytesToWrite);
-        if(rc < 0){ 
-            perror("error writing");
-        }
-
-        //WRITE STATEMENT
-        //Getting ready to send the file name in bytes
-        rc = write(sd, &inputFileName, sizeof(inputFileName));
-        if(rc < 0){ 
-            perror("error writing");
-        }
-
-
-        //WRITE STATEMENT
-        //Send contents of the file to the server one byte a a time!
-        //Total numberOfBytes written
-        int totalBytesRead = 0;
-        char byte;
-        while (1)
-        {
-            //get next byte from the InputFile
-            byte= fgetc (inputFile); // reading the file one char at a time, if it fails to get anything it'll return an EOF, which indicates end of file!
-            //Check if we've reached the end of the file, if yes that means we're done and time to break out of the loop.
-            if (byte == EOF){
-                break;
-            }
-            //Write the byte to the server
-            rc = write(sd, &byte, 1);
-            //increment number of bytes
-            totalBytesRead += 1;
-        }
-        //close the file that we've been reading from.
-        printf("Closing the file test.c\n") ;
-        fclose (inputFile ) ;
-        //READ STATEMENT ACK from Server making sure the server has recieved the proper number of bytes :)
-        int totalBytesReceived = 0;
-        rc = read(sd, &totalBytesReceived, sizeof(int));
-        printf("ACK from server: Server Received %d bytes\n", totalBytesReceived);
+    for(;;){
+        sendData(sd, server_address, from_address, from_address_len);
     }
+
 
     return 0;
 }
 
-int getFileLength(FILE* fp){
-    fseek(fp, 0, SEEK_END);
-    int size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    return size;
-}
+int sendData(int sd, struct sockaddr_in server_address, struct sockaddr_in from_address, socklen_t from_address_len){
+    int rc = 0;
+    char buffer[18];
+    char bufferIn[BUFFSIZE];
+    char message[100];
+    int maxLeng = 40;
+    char *strPtr1;
+    char *strPtr2;
+
+
+    int sequenceNumber = 0;
+
+    printf("Enter a message\n");
+    char *ptr = fgets(message, maxLeng, stdin);
+    int length = strlen(message);
+
+    int finalSequenceNumber = sequenceNumber + length;
+
+    sprintf(buffer, "%11d%4d%2s", finalSequenceNumber,length,"US");
+
+    printf("String Length: %d\n", length);
+
+    if(ptr == NULL){
+        perror("Error w/ fgets");
+        return 0;
+    }
+
+    int networkLength = htons(strlen(message));
+
+
+    rc = sendto(sd, buffer, networkLength, MSG_DONTWAIT, (struct sockaddr *) &server_address,  sizeof(server_address));
+
+    if (rc < 0){
+        perror("Error w/ sendto");
+        return 0;
+    }
+
+    return 0;
+
+};
 
 
 
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# File Transfer
+# Sliding Window Protocol
 
-Server initiates with a port number as a parameter. It will wait for a connection from a client. Then afterwards it will receive the file name and the contents of the file until done. After client connection ends, the server will go back to waiting for a client. 
+Server initiates with a port number as a parameter. It will wait for a connection from a client. Then afterwards it will receive the file name and the contents of the file until done. After client connection ends, the server will go back to waiting for a client.
 
-Client initiates with ip address and port number (of the server) as parameters. Assuming connection, it will ask the user for a file name. If the file exists it will send the name and the file's contents over to the server one byte at a time using TCP conection. Enter DONE to terminate the client. 
+Client initiates with ip address and port number (of the server) as parameters. Assuming connection, it will ask the user for a file name. If the file exists it will send the name and the file's contents over to the server one byte at a time using TCP conection. Enter DONE to terminate the client.
