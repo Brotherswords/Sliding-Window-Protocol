ee10ec186ddef0ffa5ad31146e6be723548190bd
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2022-11-03 09:48:53 -0600

    Added timeout functionality, waits for and checks for ACKs and corresponding sequence numbers

--- Client/client.c
+++ Client/client.c
@@ -21,6 +21,8 @@ int main(int argc, char *argv[]){
     char serverIP[29];
     int rc = 0;
 
+    
+
     if (argc < 3) {
         printf("Usage: %s <server ip> <port>\n", argv[0]);
         exit(1);
@@ -34,24 +36,18 @@ int main(int argc, char *argv[]){
     server_address.sin_port = htons(portNumber);
     server_address.sin_addr.s_addr = inet_addr(serverIP);
 
-    /*
-    if (connect(sd, (struct sockaddr *)&server_address, sizeof(struct sockaddr_in)) < 0) {
-        close(sd);
-        perror("error connecting stream socket");
-        exit(1);
-    }
-    */
-
     char buffer[18];
     char bufferIn[BUFFSIZE];
     char message[100];
     char leng[4];
-    int maxLeng = 40;
+    int maxLeng = 100;
     char *strPtr1;
     char *strPtr2;
 
 
     int sequenceNumber = 0;
+    printf("TEST MESSAGE #1: How are you today? I'm fine thanks for asking\n");
+    printf("TEST MESSAGE #2: How are you today? I'm fine thanks for asking!\n");
 
 
     printf("Enter a message\n");
@@ -80,51 +76,64 @@ int sendData(int sd, struct sockaddr_in server_address, struct sockaddr_in from_
 
     printf("Enter a message\n");
     int length = strlen(toSend)-1;
-    printf("toSend: %s\n", toSend);
 
     int Index1 = 0;
-    int Index2 = 1; 
+    int Index2 = 1;
 
     int seqNumberACK = 0;
-    int sequenceNumber = 0;
+    int sequenceNumberLatest = 0;
 
+
+    time_t sentTime;
+    time_t currentTime;
     while(Index1 < strlen(toSend)-1){
         memset (buffer, 0, 17);
         if (Index2 >= strlen(toSend)-1){
-            sprintf(buffer, "%11d%4d%c", sequenceNumber,1,toSend[Index1]);
+            sprintf(buffer, "%11d%4d%c", sequenceNumberLatest,1,toSend[Index1]);
         }else{
-            sprintf(buffer, "%11d%4d%c%c", sequenceNumber,2,toSend[Index1], toSend[Index2]);
+            sprintf(buffer, "%11d%4d%c%c", sequenceNumberLatest,2,toSend[Index1], toSend[Index2]);
         }
         
         printf("Index1:  %d,", Index1);
         printf("buffer %s\n", buffer);
 
+        
         int networkLength = (strlen(buffer));
-
-        int flags = 0;
+        //int flags = 0;
 
         memset (bufferIn, 0, 11);
 
-        printf("Getting ready to send :D...\n");
-        sleep(2);
+        //printf("Getting ready to send :D...\n");
+
+        sentTime = time(NULL);
         rc = sendto(sd, &buffer, networkLength, MSG_DONTWAIT, (struct sockaddr *) &server_address,  sizeof(server_address));
-        printf("SENT!!! Waiting ...\n");
-        sleep(2);
-
-        rc = recvfrom(sd, &bufferIn, 12, flags, (struct sockaddr *)&from_address,&from_address_len);
-
-        sscanf(bufferIn, "%11d", &seqNumberACK);
-        printf("ACK for index: %d\n", seqNumberACK);
-
-
+        //printf("SENT!!!\n");
 
         if (rc < 0){
             perror("Error w/ sendto");
             return 0;
         }
-        Index1+=2;
-        Index2+=2;
-        sequenceNumber+=2;
+
+        rc = recvfrom(sd, &bufferIn, 12, MSG_DONTWAIT, (struct sockaddr *)&from_address,&from_address_len);
+
+        if (rc > 0){
+            //printf("RC: %d\n", rc);
+            //printf("BufferIN: %s", bufferIn);
+            sscanf(bufferIn, "%11d", &seqNumberACK);
+            //printf("ACK# Received: %d\n", seqNumberACK);
+            //printf("Current ACK#: %d\n", sequenceNumberLatest);
+            if (seqNumberACK == sequenceNumberLatest){
+                Index1+=2;
+                Index2+=2;
+                sequenceNumberLatest+=2;
+            }
+        }
+        currentTime = time(NULL);
+        printf("Time delay: %ld \n", currentTime-sentTime);
+        if (currentTime - sentTime > 2){
+            continue;
+        }        
+
     }
 
     return 1;
